76dc623b08af1ebe8bdadccf827976d3f1b2e00d
Johannes Natter <office@dsp-crowd.com>
2023-05-12 23:01:58 +0200

    docker: Prepare for Windows 10

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-proj="processing-tutorials"
+docker build -t "processing-tutorials" .
 
-cd $(dirname $0)
-dirDocker="$(pwd)"
-
-docker build -t "$proj" --build-arg user="$USER" --build-arg uid="$(id -u)" --build-arg gid="$(id -g)" .
---
+++ t3_t2_initialize/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+meson setup result && ninja -C result
+
