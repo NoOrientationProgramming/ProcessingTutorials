52e3fedd7f15ef1c3548e36a5a4de963e372fb0c
Johannes Natter <office@dsp-crowd.com>
2023-05-15 18:27:41 +0200

    t1-6: Install Windows DLLs during build

--- t1_hello-world/cmake.sh
+++ t1_hello-world/cmake.sh
@@ -10,6 +10,12 @@ cd ..
 
 if [ ! -d "build-cmake-windows" ]; then
 	mkdir build-cmake-windows
+
+	verMinGw="$(ls -1 /usr/lib/gcc/x86_64-w64-mingw32 | grep posix | tail -1)"
+
+	cp /usr/lib/gcc/x86_64-w64-mingw32/$verMinGw/libgcc_s_seh-1.dll build-cmake-windows/.
+	cp /usr/lib/gcc/x86_64-w64-mingw32/$verMinGw/libstdc++-6.dll build-cmake-windows/.
+	cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll build-cmake-windows/.
 fi
 cd build-cmake-windows
 cmake -DCMAKE_TOOLCHAIN_FILE=../docker/dependencies/build/CMakeToolchain-win64.cmake ..
--- t1_hello-world/meson.sh
+++ t1_hello-world/meson.sh
@@ -7,6 +7,12 @@ ninja -C build-meson-ubuntu
 
 if [ ! -d "build-meson-windows" ]; then
 	meson setup --cross-file ../docker/dependencies/build/crosscompile-win64.txt build-meson-windows
+
+	verMinGw="$(ls -1 /usr/lib/gcc/x86_64-w64-mingw32 | grep posix | tail -1)"
+
+	cp /usr/lib/gcc/x86_64-w64-mingw32/$verMinGw/libgcc_s_seh-1.dll build-meson-windows/.
+	cp /usr/lib/gcc/x86_64-w64-mingw32/$verMinGw/libstdc++-6.dll build-meson-windows/.
+	cp /usr/x86_64-w64-mingw32/lib/libwinpthread-1.dll build-meson-windows/.
 fi
 ninja -C build-meson-windows
 
