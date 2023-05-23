995e3d9f829ce7174960b3d6e2a2d3a6b0ebeec3
Johannes Natter <office@dsp-crowd.com>
2023-05-23 23:26:47 +0200

    Add t10

---
+++ t10_t09_child-repelled/main.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <thread>
+#include <signal.h>
+
+#include "Supervising.h"
+
+using namespace std;
+
+Supervising *pApp = NULL;
+
+void applicationCloseRequested(int signum)
+{
+	(void)signum;
+	cout << endl;
+	pApp->unusedSet();
+}
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	levelLogSet(4);
+
+	pApp = Supervising::create();
+	if (!pApp)
+	{
+		cerr << "could not create process" << endl;
+		return 1;
+	}
+
+	signal(SIGINT, applicationCloseRequested);
+	signal(SIGTERM, applicationCloseRequested);
+
+	while (1)
+	{
+		pApp->treeTick();
+		this_thread::sleep_for(chrono::milliseconds(2));
+
+		if (pApp->progress())
+			continue;
+
+		break;
+	}
+
+	Success success = pApp->success();
+	Processing::destroy(pApp);
+
+	Processing::applicationClose();
+
+	return !(success == Positive);
+}
+
