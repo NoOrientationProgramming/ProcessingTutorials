6b885d3a928963c27ec10e106301820a3e522235
Johannes Natter <office@dsp-crowd.com>
2023-05-23 22:57:53 +0200

    Add t7

---
+++ t7_t6_pregnancy/Supervising.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+
+#include "Supervising.h"
+#include "SystemDebugging.h"
+
+using namespace std;
+
+Supervising::Supervising()
+	: Processing("Supervising")
+	, mCounter1(0)
+	, mCounter2(1)
+{}
+
+Success Supervising::initialize()
+{
+	cout << "Data initialized" << endl;
+	cout << "Service is running now" << endl;
+
+	SystemDebugging *pDbg = SystemDebugging::create(this);
+	if (!pDbg)
+		return procErrLog(-1, "could not create process");
+
+	start(pDbg);
+
+	return Positive;
+}
+
+Success Supervising::process()
+{
+	++mCounter1;
+
+	cntSecProcess();
+
+	return Pending;
+}
+
+void Supervising::cntSecProcess()
+{
+	mCounter2 += 5;
+}
+
+void Supervising::processInfo(char *pBuf, char *pBufEnd)
+{
+	dInfo("Counter 1\t\t%d\n", mCounter1);
+	dInfo("Counter 2\t\t%d\n", mCounter2);
+}
+
